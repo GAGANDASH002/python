8d01649ad68df218e4e2f9380fb3eeb194d0724e
GAGANDASH002 <gagandash002@gmail.com>
2024-03-21 16:22:44 +0530

    GUESS THE NUMBER

---
+++ python/miniproject.py
@@ -0,0 +1,21 @@
+'''GUESS THE NUMBER'''
+
+import random
+
+target=random.randint(1,100)#generates random number between  and 100
+
+while True:
+    userChoice=(input("Guess the target or Quit:"))
+    if(userChoice=="Q"):
+        break
+    userChoice=int(userChoice)
+    if(userChoice==target):
+        print("Congratulations!: You guessed correctly")
+        break
+    elif(userChoice<target):
+        print("number too small , guess again")
+    else:
+        print("number too big , guess again")
+
+print("-----GAME OVER-----")
+
