c9daa8f4f4ac1f77a90cc45ce5af6b1f71b4e2c5
GAGANDASH002 <gagandash002@gmail.com>
2024-03-27 15:12:19 +0530

    question 10 on python

--- pythongfg/10.py
+++ pythongfg/10.py
@@ -1,6 +1,6 @@
 #Join Tuples if similar initial element
 test_list = [(5, 6), (5, 7), (5, 8), (6, 10), (7, 13)]
-res=[]
+res={}
 for tup in test_list:
     key=tup[0]
     if key in res:
@@ -8,4 +8,31 @@ for tup in test_list:
     else:
         res[key]=list(tup)
 
-print(res.values)
+print(list(res.values()))
+
+#Removing duplicates from tuple
+
+tup=(1,2,3,1,4,2,5,7)
+x=[]
+
+for i in tup:
+    if i not in x:
+        x.append(i)
+
+new=tuple(x)
+
+print(new)
+
+#How to get unique elements in nested tuple
+
+test_list = [(3, 4, 5), (4, 5, 7), (1, 4)]
+
+res=[]
+
+for tup in test_list:
+    for ele in tup:
+        if ele not in res:
+            res.append(ele)
+
+res=tuple(res)
+print(res)
