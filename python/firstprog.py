f908ddfb29dcb6ec898d67b1ddf9f22f1c3b93a1
GAGANDASH002 <gagandash002@gmail.com>
2024-04-29 20:27:59 +0530

    this is my 1st python program

--- python/firstprog.py
+++ python/firstprog.py
@@ -4,25 +4,25 @@ age=20
 
 
 print("my name is",name,"my age is",age)
-print(type(name))#type function returns datatype
+print(type(name))#type function returns datatype of variable 
 print(type(age))
 
 a=5
 b=5
 sum=a+b
 print(sum)
-#arithmetic op with int and float results in float
+#arithmetic op of int and float results in float
 a=10
 b=5.0
 print(a*b)
-#int divison with float and int will givr int displayed as float
+#int divison with float and int will give int displayed as float
 a,b=20.5,3
 print(a//b)#result is same as floor(a/b)
 
 #expression execution
 a,b=2,3  
 txt="@"
-print(2*txt*3)
+print(a*txt*b)
 #concatenation
 a="2"
 b=3
